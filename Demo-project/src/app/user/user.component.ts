bd31f48bfd280e303a5bdae5ff003f85e7d57aeb
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-09-23 21:02:28 +0530

    @Input Tag along with required true

--- Demo-project/src/app/user/user.component.ts
+++ Demo-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal, computed } from '@angular/core';
+import { Component, signal, computed, Input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,22 +10,26 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+  @Input({ required: true }) avator!: string;
+  @Input({ required: true }) name!: string;
   //selectedUser =  DUMMY_USERS[randomIndex];
 
   //using signals
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser.avatar;
+  get imagePath() {
+    return 'assets/users/' + this.avator;
+  }
+
+  // onSelectedUser() {
+  //   console.log('Clicked !!!');
+  //   const onUserSelectIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  //   //this.selectedUser = DUMMY_USERS[onUserSelectIndex];
+
+  //   //using signals
+  //   this.selectedUser.set(DUMMY_USERS[onUserSelectIndex]);
   // }
 
-  onSelectedUser() {
-    console.log('Clicked !!!');
-    const onUserSelectIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    //this.selectedUser = DUMMY_USERS[onUserSelectIndex];
-
-    //using signals
-    this.selectedUser.set(DUMMY_USERS[onUserSelectIndex]);
-  }
+  onSelectedUser() {}
 }
