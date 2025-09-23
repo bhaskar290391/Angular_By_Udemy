f27e220181318f737b9e8d94ae3b78d351c4c5c0
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-09-23 22:29:32 +0530

    using input with help of signal

--- Demo-project/src/app/user/user.component.ts
+++ Demo-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal, computed, Input } from '@angular/core';
+import { Component, signal, computed, Input, input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,17 +10,23 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) avator!: string;
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) avator!: string;
+  // @Input({ required: true }) name!: string;
+
+  //using input instead of @Input
+  avator = input.required<string>();
+  name = input.required<string>();
+  imagePath = computed(() => 'assets/users/' + this.avator());
+
   //selectedUser =  DUMMY_USERS[randomIndex];
 
   //using signals
   // selectedUser = signal(DUMMY_USERS[randomIndex]);
   // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
-  get imagePath() {
-    return 'assets/users/' + this.avator;
-  }
+  // get imagePath() {
+  //   return 'assets/users/' + this.avator;
+  // }
 
   // onSelectedUser() {
   //   console.log('Clicked !!!');
